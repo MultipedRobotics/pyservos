d180149c996dcd58bed5cbc259d61c80ac14e548
walchko <walchko@users.norepy.github.com>
2018-04-19 12:20:14 -0600

    architecting new api to include more servos

---
+++ new-version/__init__.py
@@ -0,0 +1,13 @@
+# from .servoserial import ServoSerial
+# from .packet import Packet
+# from .ax12 import AX12
+# from .xl320 import XL320
+# from .xl420 import XL420
+# from .protocol import Protocol1, Protocol2  # need?
+# from .packet import Packet
+
+
+__copyright__ = 'Copyright (c) 2016 Kevin Walchko'
+__license__ = 'MIT'
+__author__ = 'Kevin J. Walchko'
+__version__ = '2.0.0'
