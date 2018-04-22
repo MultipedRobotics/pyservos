5a42ce4faf5867e3c4dee72a4a433c34f0030cee
walchko <walchko@users.noreply.github.com>
2018-04-22 12:45:05 -0600

    up

--- bin/set_angle.py
+++ bin/set_angle.py
@@ -22,12 +22,36 @@ Example: set servo 3 to angle 45
 ./set_angle /dev/serial0 45 -i 3
 """
 
+errorStr = {
+	0: 'input voltage error',
+	1: 'angle limit error',
+	2: 'overheating error',
+	3: 'range error',
+	4: 'checksum error',
+	5: 'overload error',
+	6: 'instruction error'
+}
+
+
+def printErrorStr(pkt):
+	if len(pkt) != 6:
+		print('Bad packet: {}'.format(pkt))
+	err = pkt[4]
+	if err > 0:
+		errs = []
+		for i in range(7):
+			if (err & (1 << i)):
+				errs.append(errorStr[i])
+		return errs
+	return None
+
 
 def handleArgs():
 	parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=argparse.RawTextHelpFormatter)
 	parser.add_argument('-i', '--id', help='servo id', type=int, default=0)
 	parser.add_argument('port', help='serial port  or \'dummy\' for testing', type=str)
 	parser.add_argument('angle', help='servo angle in degrees: 0.0 - 300.0', type=float)
+	parser.add_argument('-t', '--type', help='type of servo, 1:AX-12A 2:XL-320 servo 3:XL-430, default: AX-12A', type=int, default=1)
 	parser.add_argument('-s', '--speed', help='servo speed: 1 - 1023', type=int, default=0)
 
 	args = vars(parser.parse_args())
@@ -37,17 +61,28 @@ def handleArgs():
 def main():
 	args = handleArgs()
 
+	servoType = {
+		1: pyservos.AX12,
+		2: pyservos.XL320,
+		# 3: pyservos.XL430
+	}
+	servoStr = {
+		1: 'AX-12A',
+		2: 'XL-320',
+		3: 'XL-430'
+	}
+
 	ID = args['id']
 	port = args['port']
 	angle = args['angle']
 	speed = args['speed']
 
-	print('Setting servo[{}] to {:.2f} on port {}'.format(ID, angle, port))
+	print('Setting {} servo[{}] to {:.2f} on port {}'.format(servoStr[args['type']], ID, angle, port))
 
 	serial = ServoSerial(port=port)
 	serial.open()
 
-	servo = Packet(pyservos.AX12)
+	servo = Packet(servoType[args['type']])
 
 	# pkt = servo.makeServoMovePacket(ID, angle)
 	val = angle2int(angle, degrees=True) + le(speed)
@@ -55,6 +90,11 @@ def main():
 	ans = serial.sendPkt(pkt)  # send packet to servo
 	if ans:
 		print('status: {}'.format(ans))
+		err = printErrorStr(ans)
+		if err:
+			print("Error ----------")
+			for e in err:
+				print(' >', e)
 
 
 if __name__ == '__main__':
