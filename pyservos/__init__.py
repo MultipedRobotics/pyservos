ab6db1d9cb529343513d883bdeec20f6e9de235d
Kevin <walchko@users.noreply.github.com>
2020-04-20 10:08:35 -0400

    Update __init__.py

--- pyservos/__init__.py
+++ pyservos/__init__.py
@@ -3,9 +3,9 @@
 # from .xl320 import XL320
 # from .xl430 import XL430
 try:
-    from importlib_metadata import version # type: ignore
-except ImportError:
     from importlib.metadata import version # type: ignore
+except ImportError:
+    from importlib_metadata import version # type: ignore
 
 __copyright__ = 'Copyright (c) 2016 Kevin Walchko'
 __license__ = 'MIT'
